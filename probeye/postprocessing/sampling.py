adf5882ec04c86bc6128fd000006096ef8594f35
Klawonn <alexander.klawonn@bam.de>
2021-11-27 09:16:00 +0100

    re-formatted sampling.py

--- probeye/postprocessing/sampling.py
+++ probeye/postprocessing/sampling.py
@@ -1,3 +1,6 @@
+# standard library
+from typing import Union, Optional, TYPE_CHECKING
+
 # third party imports
 import arviz as az
 import numpy as np
@@ -7,74 +10,76 @@ import matplotlib.pyplot as plt
 from probeye.subroutines import len_or_one
 from probeye.subroutines import add_index_to_tex_prm_name
 
+# imports only needed for type hints
+if TYPE_CHECKING:
+    from probeye.definition.inference_problem import InferenceProblem
+
 
 def create_pair_plot(
-    inference_data,
-    problem,
-    plot_with="arviz",
-    plot_priors=True,
-    focus_on_posterior=False,
-    kind="kde",
-    figsize=(9, 9),
-    textsize=10,
-    true_values=None,
-    show_legends=True,
-    show=True,
+    inference_data: az.data.inference_data.InferenceData,
+    problem: "InferenceProblem",
+    plot_with: str = "arviz",
+    plot_priors: bool = True,
+    focus_on_posterior: bool = False,
+    kind: str = "kde",
+    figsize: Optional[tuple] = (9, 9),
+    textsize: Union[int, float] = 10,
+    true_values: Optional[dict] = None,
+    show_legends: bool = True,
+    show: bool = True,
     **kwargs,
-):
+) -> np.ndarray:
     """
     Creates a pair-plot for the given inference data using arviz.
 
     Parameters
     ----------
-    inference_data : obj[arviz.data.inference_data.InferenceData]
+    inference_data
         Contains the results of the sampling procedure.
-    problem : obj[InferenceProblem]
+    problem
         The inference problem the inference data refers to.
-    plot_with : {'arviz', 'seaborn', 'matplotlib'}, optional
-        Defines the python package the plot will be generated with.
-    plot_priors : bool, optional
+    plot_with
+        Defines the python package the plot will be generated with. Options are:
+        {'arviz', 'seaborn', 'matplotlib'}.
+    plot_priors
         If True, the prior-distributions are included in the marginal subplots.
         Otherwise the priors are not shown.
-    focus_on_posterior : bool, optional
-        If True, the marginal plots will focus on the posteriors, i.e., the
-        range of the horizontal axis will adapt to the posterior. This might
-        result in just seeing a fraction of the prior distribution (if they are
-        included). If False, the marginal plots will focus on the priors, which
-        will have a broader x-range. If plot_priors=False, this argument has no
-        effect on the generated plot.
-    kind : str, optional
+    focus_on_posterior
+        If True, the marginal plots will focus on the posteriors, i.e., the range of the
+        horizontal axis will adapt to the posterior. This might result in just seeing a
+        fraction of the prior distribution (if they are included). If False, the
+        marginal plots will focus on the priors, which will have a broader x-range. If
+        plot_priors=False, this argument has no effect on the generated plot.
+    kind
         Type of plot to display ('scatter', 'kde' and/or 'hexbin').
-    figsize : tuple, None, optional
-        Defines the size of the generated plot in the default unit. If None is
-        chose, the figsize will be derived automatically.
-    textsize : float, optional
+    figsize
+        Defines the size of the generated plot in the default unit. If None is chosen,
+        the figsize will be derived automatically.
+    textsize
         Defines the font size in the default unit.
-    true_values : None, dict, optional
-        Used for plotting 'true' parameter values. Keys are the parameter names
-        and values are the values that are supposed to be shown in the marginal
-        plots.
-    show_legends : bool, optional
-        If True, legends are shown in the marginal plots. Otherwise no legends
-        are included in the plot.
-    show : boolean, optional
-        When True, the show-method is called after creating the plot. Otherwise,
-        the show-method is not called. The latter is useful, when the plot
-        should be further processed.
+    true_values
+        Used for plotting 'true' parameter values. Keys are the parameter names and
+        values are the values that are supposed to be shown in the marginal plots.
+    show_legends
+        If True, legends are shown in the marginal plots. Otherwise no legends are
+        included in the plot.
+    show
+        When True, the show-method is called after creating the plot. Otherwise, the
+        show-method is not called. The latter is useful, when the plot should be further
+        processed.
     kwargs
         Additional keyword arguments passed to arviz' pairplot function.
 
     Returns
     -------
-    array[matplotlib.axes._subplots.AxesSubplot]
-        The subplots of the created plot.
+        The array of subplots of the created plot.
     """
 
     if plot_with == "arviz":
 
-        # set default value for kde_kwargs if not given in kwargs; note that
-        # this default value is mutable, so it should not be given as a default
-        # argument in create_pair_plot
+        # set default value for kde_kwargs if not given in kwargs; note that this
+        # default value is mutable, so it should not be given as a default argument in
+        # create_pair_plot
         if "kde_kwargs" not in kwargs:
             kwargs["kde_kwargs"] = {
                 "contourf_kwargs": {"alpha": 0},
@@ -89,17 +94,16 @@ def create_pair_plot(
                 tex = problem.parameters[prm_name].tex
                 if dim > 1:
                     if tex in inference_data.posterior.keys():
-                        # in this case, the inference_data object contains
-                        # samples of a multidimensional parameter; the
-                        # corresponding reference values must be given in a
-                        # dictionary <tex>\n0, <tex>\n1, <tex>\n2, etc.
+                        # in this case, the inference_data object contains samples of a
+                        # multidimensional parameter; the corresponding reference values
+                        # must be given in a dict <tex>\n0, <tex>\n1, <tex>\n2, etc.
                         for i in range(dim):
                             key = f"{tex}\n{i}"
                             reference_values[key] = value[i]
                     else:
-                        # in this case, the samples of a multidimensional
-                        # parameter have been already decomposed, so all the
-                        # channels in the inference data are 1D
+                        # in this case, the samples of a multidimensional parameter have
+                        # been already decomposed, so all the channels in the inference
+                        # data are 1D
                         for i in range(dim):
                             key = add_index_to_tex_prm_name(tex, i + 1)
                             reference_values[key] = value[i]
@@ -121,16 +125,16 @@ def create_pair_plot(
             **kwargs,
         )
 
-        # by default, the y-axis of the first and last marginal plot have ticks,
-        # tick-labels and axis-labels that are not meaningful to show on the
-        # y-axis; hence, we remove them here
+        # by default, the y-axis of the first and last marginal plot have ticks, tick-
+        # labels and axis-labels that are not meaningful to show on the y-axis; hence,
+        # we remove them here
         for i in [0, -1]:
             axs[i, i].yaxis.set_ticks_position("none")
             axs[i, i].yaxis.set_ticklabels([])
             axs[i, i].yaxis.set_visible(False)
 
-        # adds a reference value in each marginal plot; for some reason this is
-        # not done by arviz.pair_plot when passing 'reference_values'
+        # adds a reference value in each marginal plot; for some reason this is not done
+        # by arviz.pair_plot when passing 'reference_values'
         if "reference_values" in kwargs:
             reference_values_kwargs = None
             if "reference_values_kwargs" in kwargs:
@@ -160,14 +164,14 @@ def create_pair_plot(
                 if focus_on_posterior:
                     x_min, x_max = axs[i, i].get_xlim()
                     x = np.linspace(x_min, x_max, 200)
-                # the following code adds labels to the prior and posterior plot
-                # if they are represented as lines
+                # the following code adds labels to the prior and posterior plot if they
+                # are represented as lines
                 if axs[i, i].lines:
                     posterior_handle = [axs[i, i].lines[0]]
                     posterior_label = ["posterior"]
                 else:
-                    # this is for the case, when the posterior is not shown as
-                    # a line, but for example as a histogram etc.
+                    # this is for the case, when the posterior is not shown as a line,
+                    # but for example as a histogram etc.
                     posterior_handle, posterior_label = [], []
                 problem.parameters[prm_name].prior.plot(
                     axs[i, i], problem.parameters, x=x
@@ -181,8 +185,7 @@ def create_pair_plot(
                     )
                 i += 1
 
-            # here, the axis of the non-marginal plots are adjusted to the new
-            # axis ranges
+            # here, the axis of the non-marginal plots are adjusted to the new ranges
             if not focus_on_posterior:
                 n = len(prm_names)
                 for i in range(n):
@@ -193,8 +196,8 @@ def create_pair_plot(
                         axs[i, j].set_ylim((x_min, x_max))
         else:
 
-            # the following code adds legends to the marginal plots for the case
-            # where no priors are supposed to be plotted
+            # the following code adds legends to the marginal plots for the case where
+            # no priors are supposed to be plotted
             if show_legends:
                 prm_names = problem.get_theta_names(tex=False, components=True)
                 for i, prm_name in enumerate(prm_names):
@@ -205,8 +208,8 @@ def create_pair_plot(
                         posterior_handle = [axs[i, i].lines[0]]
                         posterior_label = ["posterior"]
                     else:
-                        # this is for the case, when the posterior is not shown
-                        # as a line, but for example as a histogram etc.
+                        # this is for the case, when the posterior is not shown as a
+                        # line, but for example as a histogram etc.
                         posterior_handle, posterior_label = [], []
                     axs[i, i].legend(
                         posterior_handle + existing_handles,
@@ -231,21 +234,21 @@ def create_pair_plot(
 
     else:
         raise RuntimeError(
-            f"Invalid 'plot_with' argument: '{plot_with}'. Available options "
-            f"are currently 'arviz', 'seaborn', 'matplotlib'"
+            f"Invalid 'plot_with' argument: '{plot_with}'. Available options are "
+            f"currently 'arviz', 'seaborn', 'matplotlib'"
         )
 
 
 def create_posterior_plot(
-    inference_data,
-    problem,
-    plot_with="arviz",
-    kind="hist",
-    figsize=(10, 3),
-    textsize=10,
-    hdi_prob=0.95,
-    true_values=None,
-    show=True,
+    inference_data: az.data.inference_data.InferenceData,
+    problem: "InferenceProblem",
+    plot_with: str = "arviz",
+    kind: str = "hist",
+    figsize: Optional[tuple] = (9, 9),
+    textsize: Union[int, float] = 10,
+    hdi_prob: float = 0.95,
+    true_values: Optional[dict] = None,
+    show: bool = True,
     **kwargs,
 ):
     """
@@ -253,36 +256,35 @@ def create_posterior_plot(
 
     Parameters
     ----------
-    inference_data : obj[arviz.data.inference_data.InferenceData]
+    inference_data
         Contains the results of the sampling procedure.
-    problem : obj[InferenceProblem]
+    problem
         The inference problem the inference data refers to.
-    plot_with : {'arviz', 'seaborn', 'matplotlib'}, optional
-        Defines the python package the plot will be generated with.
-    kind : str, optional
+    plot_with
+        Defines the python package the plot will be generated with. Options are:
+        {'arviz', 'seaborn', 'matplotlib'}.
+    kind
         Type of plot to display ('kde' or 'hist').
-    figsize : tuple, None, optional
-        Defines the size of the generated plot in the default unit. If None is
-        chose, the figsize will be derived automatically.
-    textsize : float, optional
+    figsize
+        Defines the size of the generated plot in the default unit. If None is chosen,
+        the figsize will be derived automatically.
+    textsize
         Defines the font size in the default unit.
-    hdi_prob : float, optional
+    hdi_prob
         Defines the highest density interval. Must be a number between 0 and 1.
-    true_values : None, dict, optional
-        Used for plotting 'true' parameter values. Keys are the parameter names
-        and values are the values that are supposed to be shown in the marginal
-        plots.
-    show : boolean, optional
-        When True, the show-method is called after creating the plot. Otherwise,
-        the show-method is not called. The latter is useful, when the plot
-        should be further processed.
+    true_values
+        Used for plotting 'true' parameter values. Keys are the parameter names and
+        values are the values that are supposed to be shown in the marginal plots.
+    show
+        When True, the show-method is called after creating the plot. Otherwise, the
+        show-method is not called. The latter is useful, when the plot should be further
+        processed.
     kwargs
         Additional keyword arguments passed to arviz' plot_posterior function.
 
     Returns
     -------
-    array[matplotlib.axes._subplots.AxesSubplot]
-        The subplots of the created plot.
+        The array of subplots of the created plot.
     """
 
     if plot_with == "arviz":
@@ -322,19 +324,19 @@ def create_posterior_plot(
 
     else:
         raise RuntimeError(
-            f"Invalid 'plot_with' argument: '{plot_with}'. Available options "
-            f"are currently 'arviz', 'seaborn', 'matplotlib'"
+            f"Invalid 'plot_with' argument: '{plot_with}'. Available options are "
+            f"currently 'arviz', 'seaborn', 'matplotlib'"
         )
 
 
 def create_trace_plot(
-    inference_data,
-    problem,
-    plot_with="arviz",
-    kind="trace",
-    figsize=(10, 6),
-    textsize=10,
-    show=True,
+    inference_data: az.data.inference_data.InferenceData,
+    problem: "InferenceProblem",
+    plot_with: str = "arviz",
+    kind: str = "trace",
+    figsize: Optional[tuple] = (10, 6),
+    textsize: Union[int, float] = 10,
+    show: bool = True,
     **kwargs,
 ):
     """
@@ -342,38 +344,38 @@ def create_trace_plot(
 
     Parameters
     ----------
-    inference_data : obj[arviz.data.inference_data.InferenceData]
+    inference_data
         Contains the results of the sampling procedure.
-    problem : obj[InferenceProblem]
+    problem
         The inference problem the inference data refers to.
-    plot_with : {'arviz', 'seaborn', 'matplotlib'}, optional
-        Defines the python package the plot will be generated with.
-    kind: str, optional
-        Allows to choose between plotting sampled values per iteration ("trace")
-        and rank plots ("rank_bar", "rank_vlines").
-    figsize : tuple, None, optional
-        Defines the size of the generated plot in the default unit. If None is
-        chose, the figsize will be derived automatically.
-    textsize : float, optional
+    plot_with
+        Defines the python package the plot will be generated with. Options are:
+        {'arviz', 'seaborn', 'matplotlib'}.
+    kind
+        Allows to choose between plotting sampled values per iteration ("trace") and
+        rank plots ("rank_bar", "rank_vlines").
+    figsize
+        Defines the size of the generated plot in the default unit. If None is chose,
+        the figsize will be derived automatically.
+    textsize
         Defines the font size in the default unit.
-    show : boolean, optional
-        When True, the show-method is called after creating the plot. Otherwise,
-        the show-method is not called. The latter is useful, when the plot
-        should be further processed.
+    show
+        When True, the show-method is called after creating the plot. Otherwise, the
+        show-method is not called. The latter is useful, when the plot should be further
+        processed.
     kwargs
-        Additional keyword arguments passed to arviz' plot_posterior function.
+        Additional keyword arguments passed to arviz' plot_trace function.
 
     Returns
     -------
-    array[matplotlib.axes._subplots.AxesSubplot]
-        The subplots of the created plot.
+        The array of subplots of the created plot.
     """
 
     if plot_with == "arviz":
 
-        # set default value for kde_kwargs if not given in kwargs; note that
-        # this default value is mutable, so it should not be given as a default
-        # argument in create_pair_plot
+        # set default value for kde_kwargs if not given in kwargs; note that this
+        # default value is mutable, so it should not be given as a default argument in
+        # create_pair_plot
         if "plot_kwargs" not in kwargs:
             kwargs["plot_kwargs"] = {"textsize": textsize}
 
@@ -394,6 +396,6 @@ def create_trace_plot(
 
     else:
         raise RuntimeError(
-            f"Invalid 'plot_with' argument: '{plot_with}'. Available options "
-            f"are currently 'arviz', 'seaborn', 'matplotlib'"
+            f"Invalid 'plot_with' argument: '{plot_with}'. Available options are "
+            f"currently 'arviz', 'seaborn', 'matplotlib'"
         )
